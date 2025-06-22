5dbd7fe67608a84cd8222d610e756a0fefcdc596
Akshay Pandit <akshaycpandit19@gmail.com>
2025-06-22 20:28:42 +0530

    Add Subscription module to app Module root for Dependency injection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PaymentsModule } from './payments/payments.module';
+import { SubscriptionsModule } from './subscriptions/subscriptions.module';
 
 @Module({
-  imports: [PaymentsModule],
+  imports: [PaymentsModule, SubscriptionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
